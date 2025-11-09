b74ff1622623e12fb94ca0c516fc17f165575141
Anjal Sotang <anjalsotang26@gmail.com>
2025-11-09 15:33:14 +0545

    Completed

--- Backend/index.js
+++ Backend/index.js
@@ -130,6 +130,7 @@ socketService.initialize(io);
 // Use the PORT value from the .env file or default to 3000
 const PORT = process.env.PORT || 3000;
 
+console.log(PORT)
 // Start server - IMPORTANT: Use server.listen instead of app.listen
 server.listen(PORT, () => { 
   console.log(`Server is running at http://localhost:${PORT}`);
--- Frontend/src/http/index.js
+++ Frontend/src/http/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'http://localhost:3001/',
+    baseURL: 'https://fittrackbackend-h5oz.onrender.com/', // <-- updated
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
